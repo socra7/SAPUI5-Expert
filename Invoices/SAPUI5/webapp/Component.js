61c2d3b8f13bd8fb5cc34e6d666754451114736e
socra7 <geschaeft7@gmail.com>
2022-05-23 20:45:59 +0000

    05 Estructura Proyectos SAPUI5

---
+++ Invoices/SAPUI5/webapp/Component.js
@@ -0,0 +1,43 @@
+sap.ui.define([
+
+    "sap/ui/core/UIComponent",
+    "logaligroup/SAPUI5/model/Models",
+    "sap/ui/model/resource/ResourceModel"
+
+],
+
+    /**
+     * @param {typeof sap.ui.core.UIComponent} UIComponent
+     */
+
+    function (UIComponent, Models, ResourceModel) {
+
+        return UIComponent.extend("logaligroup.SAPUI5.Component", {
+
+            metadata: {
+
+                manifest: "json"
+
+            },
+            
+            init: function () {
+
+                // call the init function of the parent
+                UIComponent.prototype.init.apply(this, arguments);
+
+                // set data model on the view
+                this.setModel(Models.createRecipient());
+
+                //set i18n on the view
+                var i18nModel = new ResourceModel({
+                    bundleName: "logaligroup.SAPUI5.i18n.i18n"
+                });
+                this.setModel(i18nModel, "i18n");
+
+            }
+
+
+        });
+
+
+    })
---
+++ Invoices/SAPUI5/webapp/controller/App.controller.js
@@ -0,0 +1,40 @@
+// @ts-nocheck
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+
+],
+
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     * @param {typeof sap.m.MessageToast} MessageToast 
+     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel 
+
+
+     */
+
+    function (Controller, MessageToast, Models, ResourceModel) {
+        'use strict';
+
+        return Controller.extend("logaligroup.SAPUI5.controller.App", {
+
+            onInit: function () {
+
+
+
+            },
+
+            onShowHello: function () {
+
+                //read text from i18n
+                var oBundle = this.getView().getModel("i18n").getResourceBundle();
+                //read property from data model
+                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+                var sMgs = oBundle.getText("helloMsg", [sRecipient]);
+                MessageToast.show(sMgs);
+
+            }
+        });
+
+    });
